67abffdc243e13ac4ac1ec1038684b40960ea61a
donia moradi <doniamorady00@gmail.com>
2026-08-23 00:35:59 +0430

    chore:add photo and status col to the room migration

--- database/migrations/2026_08_22_180758_create_rooms_table.php
+++ database/migrations/2026_08_22_180758_create_rooms_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('imgae');
+            $table->boolean('status')->default(true);
             $table->timestamps();
             $table->softDeletes();
         });
