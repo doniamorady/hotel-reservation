c57430bfebefffcfc4569bea954f2de227bc7917
donia moradi <doniamorady00@gmail.com>
2026-08-23 10:15:49 +0430

    chore: User model and migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('fname');
-            $table->string('lname');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->text('avatar')->nullable();
             $table->string('email')->unique();
             $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
