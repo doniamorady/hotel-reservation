16b1a3c231aac3648b43fd35ba2adcc3d6b98a34
donia moradi <doniamorady00@gmail.com>
2026-09-02 15:56:36 +0430

    chore: add admin assets

---
+++ frontend/src/master.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('admin.layouts.head-tag')
+    @yield('head-tag')
+
+</head>
+
+<body dir="rtl">
+
+    @include('admin.layouts.header')
+
+
+
+    <section class="body-container">
+
+        @include('admin.layouts.sidebar')
+
+
+        <section id="main-body" class="main-body">
+
+            @yield('content')
+
+        </section>
+    </section>
+
+
+    @include('admin.layouts.scripts')
+    @yield('script')
+
+
+    <div class="toast-wrapper flex-row-reverse">
+        @include('admin.alerts.toast.error')
+        @include('admin.alerts.toast.success')
+        @include('admin.alerts.toast.update')
+    </div>
+
+    @include('admin.alerts.sweetalert.error')
+    @include('admin.alerts.sweetalert.success')
+    @include('admin.alerts.sweetalert.delete-confirm')
+
+
+    @include('admin.alerts.sweetalert.delete-confirm')
+
+
+
+</body>
+
+</html>
