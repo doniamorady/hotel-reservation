08bfb7c39ec12cc60ebb40e5fbe538bba61f2faf
donia moradi <doniamorady00@gmail.com>
2026-09-03 10:36:11 +0430

    feat: gallery migration,model,relationship

--- backend/app/Models/Room.php
+++ backend/app/Models/Room.php
@@ -21,4 +21,8 @@ class Room extends Model
     {
         return $this->hasMany(Booking::class);
     }
+    
+    public function gallery(){
+        return $this->hasMany(Gallery::class);
+    }
 }
